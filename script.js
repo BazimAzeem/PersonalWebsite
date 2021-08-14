45ee0a81923f7f7f6b599a3f668034afe65df3d4
BazimAzeem <bazimazeem123@gmail.com>
2021-08-14 18:51:33 +0530

    Introduced frosted glass effect

--- script.js
+++ script.js
@@ -6,22 +6,4 @@ switcher.addEventListener('click', function() {
 
     var className = document.body.className;
     console.log("I am currently in " + className);
-
-    var image = document.getElementById("home-image");
-    if (className == "light-theme") {
-        image.src = "images/trees-light.png";
-        image.alt = "Light themed trees";
-    } else {
-        image.src = "images/trees-dark.png";
-        image.alt = "Dark themed trees";
-    }
 });
-
-window.onscroll = function() {
-    var navBar = document.getElementById("nav-bar");
-    if (window.pageYOffset > window.innerHeight) {
-        navBar.classList.add("nav-scroll");
-    } else {
-        navBar.classList.remove("nav-scroll");
-    }
-};
