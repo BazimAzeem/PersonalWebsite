043689b16164852f34d70b4d1a642597fe110810
BazimAzeem <bazimazeem123@gmail.com>
2022-05-11 21:21:27 -0400

    Made navigation bar responsive

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 'use strict';
 
-const switcher = document.querySelector('#theme-toggle');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+// Theme toggle
+const theme_toggle = document.querySelector('.nav-bar__button--theme-toggle');
+theme_toggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
 
     var className = document.body.className;
-    console.log("I am currently in " + className);
+    console.log("The theme is " + className);
+});
+
+// Menu toggle
+const menu_toggle = document.querySelector('.nav-bar__button--menu-toggle');
+menu_toggle.addEventListener('click', function() {
+    var nav_bar_links = document.querySelector('.nav-bar__links');
+    if (nav_bar_links.style.maxHeight) {
+        nav_bar_links.style.maxHeight = null;
+    } else {
+        nav_bar_links.style.maxHeight = nav_bar_links.scrollHeight + "px";
+    } 
+      console.log("height = " + nav_bar_links.style.maxHeight);
 });
